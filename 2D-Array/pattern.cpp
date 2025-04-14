8a34df9bb0ff8e1a85dc76de7d7e49e8ecbb2bc1
Mann Patel <169169782+mannpatel08@users.noreply.github.com>
2025-04-14 17:09:22 +0530

    update

--- 2D-Array/pattern.cpp
+++ 2D-Array/pattern.cpp
@@ -3,28 +3,9 @@ using namespace std;
 
 int main()
 {
-    int i;
-    int j;
-
-    // for ( i = 1; i <= 5 ; i++)
-    // {
-    //     for (j = 1; j <= 5; j++)
-    //     {
-    //         cout<<j<<"  ";
-    //     }
-    //     cout<<endl;
-        
-    // }
-
-    // for (i = 1; i <= 5; i++)
-    // {
-    //     for (j = 1; j <= i; j++)
-    //     {
-    //         cout<<j << "  ";        
-    //     }
-    //     cout<<endl;
-
-
+    int i; // for rows
+    int j; // for columns
+    int k; // for spacing
 
 
 
@@ -43,25 +24,63 @@ int main()
 
     // Question 2
 
-        int num=11;
-        int row=4;
+        // int num=11;
+        // int row=4;
 
-        for (i = 1; i <= row; i++)
-        {
-            for (j = 1; j <= i; j++)
-            {
-                cout<<num<<"  ";
-                num++;
-            }
-            cout<<endl;
+        // for (i = 1; i <= row; i++)
+        // {
+        //     for (j = 1; j <= i; j++)
+        //     {
+        //         cout<<num<<"  ";
+        //         num++;
+        //     }
+        //     cout<<endl;
             
-        }
+        // }
         
         
+    // Question 3
+
+        // for (i = 5; i >= 1 ; i--)
+        // {
+        //     for (k=1 ; k <= i ; k++)
+        //     {
+        //         cout<<"  ";
+        //     }
+
+        //     for(j=i; j<=5 ; j++)
+        //     {
+        //         cout<<j<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+
+    // // Questions 4
 
 
+    //      for (i=5 ; i >= 1 ; i--)
+    //      {
+    //         for(k=0 ; k < 5-i ; k++)
+    //         {
+    //             cout<<"  ";
+    //         }
+    //         for (j=1 ; j <= i ; j++)
+    //         {
+    //             cout<<j%2<<" ";
+    //         }
+    //         cout<<endl;   
+    //     }
+        
 
 
     }
     
-    
+ 
+
+
+//  10101
+//   1010
+//    101
+//     10
+//      1
